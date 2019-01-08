f3a7ff7443b6a0799a189a7d557cf204e8b35bb4
Thiago Rabelo <thiagorabelo1@gmail.com>
2019-01-07 23:44:42 -0300

    Adicionado TODOs

--- ostream.py
+++ ostream.py
@@ -22,15 +22,55 @@ class OStream(object):  # pylint: disable=useless-object-inheritance
         return self
 
     def write(self, stream):
-        return self._output.write(stream)
+        self._output.write(stream)
+        return self
+
+    def put(self, char):
+        self._output.write(self._to_string(char)[0])
+        return self
 
     def flush(self):
         return self._output.flush()
+
+    # TODO: http://www.cplusplus.com/reference/ios/ios_base/precision/
+    def precision(self, prec=None):
+        # retornar a precisão anterior
+        raise NotImplementedError("Not yet implemented.")
+
+    # TODO: http://www.cplusplus.com/reference/ios/ios_base/width/
+    def width(self, width_=None):
+        # retorna o width anterior
+        raise NotImplementedError("Not yet implemented.")
+
+    # TODO: http://www.cplusplus.com/reference/ios/ios/fill/
+    def fill(self, char=None):
+        # retorna o fill anterior
+        raise NotImplementedError("Not yet implemented.")
+
 
 def endl(stream):
     stream.write('\n')
     stream.flush()
     return stream
+
+
+# TODO: http://www.cplusplus.com/reference/iomanip/setfill/
+def setfill(stream):
+    raise NotImplementedError("Not yet implemented.")
+
+
+# TODO: http://www.cplusplus.com/reference/iomanip/setprecision/
+# TODO: implement std::fixed?
+def setprecision(stream):
+    raise NotImplementedError("Not yet implemented.")
+
+
+# TODO: http://www.cplusplus.com/reference/iomanip/setw/
+def setw(stream):
+    raise NotImplementedError("Not yet implemented.")
+
+
+# TODO: http://www.cplusplus.com/reference/iomanip/
 
 
 if __name__ == '__main__':
